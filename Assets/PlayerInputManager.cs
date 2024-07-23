7c67c30e4b5a8fe5d14488dbd6b292dced3f56b7
Tindavetran10 <102245594+Tindavetran10@users.noreply.github.com>
2024-07-23 17:42:23 +0700

    Adding the player Input System named as PlayerController

--- Assets/PlayerInputManager.cs
+++ Assets/PlayerInputManager.cs
@@ -1,11 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInputManager : MonoBehaviour
 {
+    
     //Goals:
     // 1. Read input from the player
     // 2. Move the player based on the input
     
+    PlayerController _playerController;
+
+    [SerializeField] private Vector2 movementInput;
+
+    private void OnEnable()
+    {
+        if(_playerController == null)
+        {
+            _playerController = new PlayerController();
+            
+            // give the input value from the Movement in the PlayerController to the movementInput
+            _playerController.PlayerMovement.Movement.performed += ctx => movementInput = ctx.ReadValue<Vector2>();
+        }
+        _playerController.Enable();
+    }
 }
