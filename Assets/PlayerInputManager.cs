c54e12a57552f9761d318e034c750672f9a90f28
Tindavetran10 <102245594+Tindavetran10@users.noreply.github.com>
2024-07-23 17:29:52 +0700

    Add Odin

---
+++ Assets/PlayerInputManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputManager : MonoBehaviour
+{
+    //Goals:
+    // 1. Read input from the player
+    // 2. Move the player based on the input
+    
+}
